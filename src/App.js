08a470728c38e3ce85a7f1fd0947f478c4250a5c
Andrew Dalba <andrewdalba@gmail.com>
2020-10-25 17:25:35 -0400

    create test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Test from './components/Test/Test.js';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Test></Test>
     </div>
   );
 }
