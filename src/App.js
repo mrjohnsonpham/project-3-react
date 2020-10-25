e1fbeaaccff8bee5ef6c29e4bc960461deaf2400
Andrew Dalba <andrewdalba@gmail.com>
2020-10-25 19:12:42 -0400

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-import 'semantic-ui-css/semantic.min.css'
+import 'semantic-ui-css/semantic.min.css';
 import Test from './components/Test/Test.js';
 import AndrewsCoolButton from './components/Button/Button.js';
+import Navbar from './components/Nav/Navbar.js';
 
 function App() {
   return (
@@ -9,10 +10,11 @@ function App() {
       <header className="App-header">
         <h1>Jivko is the man!</h1>
       </header>
+      <Navbar></Navbar>
       <Test></Test>
       <AndrewsCoolButton anything="Push me baby " jivko="something"></AndrewsCoolButton>
       <AndrewsCoolButton></AndrewsCoolButton>
-      
+
     </div>
   );
 }
